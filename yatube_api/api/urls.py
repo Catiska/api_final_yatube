310b72ae7ce6986a0e72f972e3db8e2be57836ed
Catiska <katrinka5@yandex.ru>
2023-02-04 20:32:06 +0300

    Fix urls

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView, TokenVerifyView)
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
@@ -14,11 +12,6 @@ router_v1.register(r'posts/(?P<post_id>\d+)/comments',
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    # path('v1/jwt/', include('djoser.urls')), # хз, как заставить это работать
-    # path('v1/jwt/', include('djoser.urls.jwt')),  # идеи уже закончились
-    path('v1/jwt/create/', TokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
-    path('v1/jwt/refresh/', TokenRefreshView.as_view(),
-         name='token_refresh'),
-    path('v1/jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
